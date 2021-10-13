97f8b5b8d85102f59c129b37f1a6f76c6bedd9b7
piyushchaudhari04 <piyush.chaudhari@helpshift.com>
2021-10-14 00:29:49 +0530

    Add basic user creation functionality

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+                                        PermissionsMixin
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        """
+        Creates and saves a new user
+        :param email:
+        :param password:
+        :param extra_fields:
+        :type extra_fields:
+        :return: user
+        """
+        if not email:
+            raise ValueError("Users email cannot be empty")
+
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser,PermissionsMixin):
+    """
+    Custom user model that supports email instead of username
+    """
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+
+
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,37 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """
+        Test create a new user with an email is successful
+        """
+        email = "test@piyush.com"
+        password = "pass123"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """
+        Test the email for the new user is normalized
+        """
+        email = "test@PIYUSH.COM"
+        user = get_user_model().objects.create_user(email, "test123")
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """
+        Creating user with no email should raise error
+        :return:
+        :rtype:
+        """
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, "test123")
